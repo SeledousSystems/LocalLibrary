95831e0902bf231d26234a2bc35501889b6b558c
SeledousSystems <applevillas@gmail.com>
2019-08-31 19:04:01 +1200

    changes

--- routes/catalog.js
+++ routes/catalog.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-
 // Require our controllers.
 var book_controller = require('../controllers/bookController');
 var author_controller = require('../controllers/authorController');
